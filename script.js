ae917b5acbbaa7fd900ccd53f35e8173072c795a
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-07-26 16:11:01 -0500

    Add DOM elementsstyled new clasess and eventlisteners

---
+++ script.js
@@ -0,0 +1,9 @@
+let menuIcon=document.querySelector(".menuicon");
+let menuli=document.querySelector(".menuli1");
+let menuItem=document.querySelector(".menuitem1");
+
+
+menuIcon.addEventListener('click', () => {
+  menuli.classList.add(("menuli1-1"));
+  menuItem.classList.add("menuitem1-1");
+});
