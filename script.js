e63a983c82e90152f658c5d13c757c373b435951
AlexHamn <agalexandro16@gmail.com>
2021-07-29 09:39:40 -0500

    add jS popup function and project section function

--- script.js
+++ script.js
@@ -29,3 +29,143 @@ menuItem3.addEventListener("click", () => {
   menu.classList.remove("menu");
   menu.classList.add("hidden");
 });
+
+const portfolio = document.querySelector('#portfolio');
+
+const body = document.body;
+  const article = document.createElement('article');
+    const div0 = document.createElement('div');
+      const div1 = document.createElement('div');
+        const div2 = document.createElement('div');
+          const h3 = document.createElement('h3');
+          const i0 = document.createElement('i');
+        const img0 = document.createElement('img');
+        const p0 = document.createElement('p');
+      const ul = document.createElement('ul');
+        const li0 = document.createElement('li');
+        const li1 = document.createElement('li');
+        const li2 = document.createElement('li');
+        const li3 = document.createElement('li');
+      const button0 = document.createElement('button');
+        const p1 = document.createElement('p');
+        const a1 = document.createElement('a');
+          const i1 = document.createElement('i');
+      const button1 = document.createElement('button');
+        const p2 = document.createElement('p');
+        const a2 = document.createElement('a');
+          const img1 = document.createElement('img');
+
+
+
+let names = ['name1', 'name2', 'name3'];
+let images = [
+  'https://picsum.photos/684/448?grayscale',
+  'https://picsum.photos/684/448?grayscale',
+  'https://picsum.photos/684/448?grayscale'];
+let descriptions = [
+  'I really dont want to input such a long text into an array, I think it makes the code look awfull, but it\'s a requirement so I guess I\'ll just do it and don\'t complain about it cause I\'m already waaaay too delayed with this project; also, writing this text helps me practice my touchtyping so I guess that is a silverlining of all of this, anyway, I hate to write paragraphs in arrays :)',
+  'I really dont want to input such a long text into an array, I think it makes the code look awfull, but it\'s a requirement so I guess I\'ll just do it and don\'t complain about it cause I\'m already waaaay too delayed with this project; also, writing this text helps me practice my touchtyping so I guess that is a silverlining of all of this, anyway, I hate to write paragraphs in arrays :)',
+  'I really dont want to input such a long text into an array, I think it makes the code look awfull, but it\'s a requirement so I guess I\'ll just do it and don\'t complain about it cause I\'m already waaaay too delayed with this project; also, writing this text helps me practice my touchtyping so I guess that is a silverlining of all of this, anyway, I hate to write paragraphs in arrays :)'];
+let technologies = ['flash', 'html', 'jS', 'css', 'Ruby on Rails', 'bootstrap'];
+let liveLinks = [
+  'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+  'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+  'https://www.youtube.com/watch?v=dQw4w9WgXcQ'];
+let sourceLinks = [
+  'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+  'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+  'https://www.youtube.com/watch?v=dQw4w9WgXcQ'];
+
+function popUp() {
+
+  body.append(article);
+
+  article.append(div0);
+
+  div0.append(div1, ul, button0, button1);
+
+  div1.append(div2, img0, p0);
+
+  div2.append(h3, i0);
+
+  ul.append(li0, li1, li2);
+
+  button0.append(p1, a1);
+
+  a1.append(i1);
+
+  button1.append(p2, a2);
+
+  a2.append(img1);
+
+
+  article.setAttribute('class', 'backdrop');
+  div0.setAttribute('class', 'container popUp');
+  div1.setAttribute('class', 'popUp-content');
+  div2.setAttribute('class', 'container popUp-titleContainer');
+  h3.setAttribute('class', 'popUp-title');
+  i0.setAttribute('class', 'material-icons closeIcon');
+  i0.setAttribute('onclick', 'article.remove()');
+  img0.setAttribute('src', images[0]);
+  img0.setAttribute('alt', 'placeholder');
+  ul.setAttribute('class', 'container languages');
+  button0.setAttribute('class', 'popUp-button');
+  a1.setAttribute('href', liveLinks[0]);
+  i1.setAttribute('class', 'material-icons');
+  button1.setAttribute('class', 'popUp-button');
+  a2.setAttribute('href', sourceLinks[1]);
+  img1.setAttribute('src', 'media/icons/github.svg');
+  img1.setAttribute('alt', 'github');
+
+  h3.textContent = names[0];
+  i0.textContent = 'close';
+  p0.textContent = descriptions[0];
+  li0.textContent = technologies[0];
+  li1.textContent = technologies[1];
+  li2.textContent = technologies[2];
+  p1.textContent = 'See Live';
+  i1.textContent = 'stream';
+  p2.textContent = 'See Source';
+}
+
+function project() {
+
+  const article = document.createElement('article');
+    const div0 = document.createElement('div');
+          const h3 = document.createElement('h3');
+        const img0 = document.createElement('img');
+        const p0 = document.createElement('p');
+      const ul = document.createElement('ul');
+        const li0 = document.createElement('li');
+        const li1 = document.createElement('li');
+        const li2 = document.createElement('li');
+        const li3 = document.createElement('li');
+      const button0 = document.createElement('button');
+
+  portfolio.append(article);
+
+  article.append(img0, div0);
+
+  div0.append(h3, p0, ul, button0);
+
+  ul.append(li0, li1, li2, li3);
+
+  article.setAttribute('class', 'container project');
+  img0.setAttribute('src', images[0]);
+  img0.setAttribute('alt', 'project picture');
+  div0.setAttribute('class', 'container project-text');
+  ul.setAttribute('class', 'container languages');
+  button0.setAttribute('onclick', 'popUp()');
+
+  h3.textContent = names[0];
+  p0.textContent = descriptions[0];
+  li0.textContent = technologies[0];
+  li1.textContent = technologies[1];
+  li2.textContent = technologies[2];
+  li3.textContent = technologies[3];
+  button0.textContent = 'See Project';
+}
+
+for (let i = 0; i < names.length; i++) {
+  project();
+}
